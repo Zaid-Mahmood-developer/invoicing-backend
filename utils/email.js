e2b544f88d79f9ce6e6bd9939d8786e5f2ba744a
Zaid-Mahmood-developer <zaidmahmood.030@gmail.com>
2025-12-06 15:25:01 +0500

    Update SMTP host in email transporter configuration and sender email address for password reset

--- utils/email.js
+++ utils/email.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  host: "live.smtp.mailtrap.io",
+  host: "sandbox.smtp.mailtrap.io",
   port: 587,
   auth: {
     user: process.env.MAILTRAP_USER,
--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -4,7 +4,7 @@ export const sendPasswordResetEmail = async (toEmail, resetURL) => {
   const resetLink = `${process.env.CLIENT_URL}/reset-password/${resetURL}`;
   const htmlContent = RESET_PASSWORD_TEMPLATE.replace("{{resetURL}}", resetLink);
   const mailOptions = {
-    from: '"DevOx Syndicate" <noreply@mailtrap.io>',
+    from: '"DevOx Syndicate" <noreply@devox-syndicate.com>',
     to: toEmail,
     subject: "Reset Your Password",
     html: htmlContent,
